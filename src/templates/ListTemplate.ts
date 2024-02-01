03247fbc79534e94429c9375a3dd1f428364c81e
Gnanadeep-Settykara <gsettyka@asu.edu>
2024-01-31 18:20:30 -0700

    Code changes

--- src/templates/ListTemplate.ts
+++ src/templates/ListTemplate.ts
@@ -9,6 +9,8 @@ interface DOMList {
 export default class ListTemplate implements DOMList {
 
     ul: HTMLUListElement
+    
+    static instance: ListTemplate = new ListTemplate
 
     private constructor () {
         this.ul = document.getElementById("listItems") as HTMLUListElement
@@ -41,6 +43,18 @@ export default class ListTemplate implements DOMList {
             label.htmlFor = item.id
             label.textContent = item.item
             li.append(label)
+
+            const button = document.createElement("button") as HTMLButtonElement
+            button.className = 'button'
+            button.textContent = 'X'
+            li.append(button)
+
+            button.addEventListener('click', () => {
+                fullList.removeItem(item.id)
+                this.render(fullList)
+            })
+
+            this.ul.append(li)
         })
     }
 }
